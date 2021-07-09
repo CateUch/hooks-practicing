32e6676dfc54e29edb1db0f4b05617e76312e8eb
Cate Uch <cat.uch@yandex.ru>
2021-07-10 01:40:43 +0300

    useEffect, debouncing, cleanup function

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+ useEffect( () => {
+  const loggingStatus = localStorage.getItem('isLogging');
+  if (loggingStatus === '1') {
+    setIsLoggedIn(true)
+  }
+ }, []);
+
+  const loginHandler = (email, password) => {
+    // email and password should be checked there
+    localStorage.setItem('isLogging', '1')
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLogging')
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
